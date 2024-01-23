d6a4ef0a97e869e19028fdf108775e64e8553c6a
David Villaplana <davidvillaplana23@gmail.com>
2024-01-23 09:59:37 +0100

    Modificado index.js

--- src/index.js
+++ src/index.js
@@ -15,8 +15,8 @@ app.get('/about',(req,res)=>{
 })
 app.get('/env', (req, res) => {
     const t1 = "<h1>HOST: "+process.env.MYSQL_HOST+"</h1>"
-    const t2 = "<h1>HOST: "+process.env.MYSQL_USER+"</h1>"
-    const t3 = "<h1>HOST: "+process.env.MYSQL_PASS+"</h1>"
+    const t2 = "<h1>USER: "+process.env.MYSQL_USER+"</h1>"
+    const t3 = "<h1>PASS: "+process.env.MYSQL_PASS+"</h1>"
     res.send(t1+t2+t3)
 })
 console.log(process.env.MYSQL_HOST)
